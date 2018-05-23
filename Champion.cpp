b726272e4df7d098f6879892be6a20805a8ed112
Yarin Knafo <yarin665@gmail.com>
2018-05-23 20:04:24 +0300

    Update Champion.cpp

--- Champion.cpp
+++ Champion.cpp
@@ -1,20 +1,19 @@
-//
-// Created by yarin665 on 21/05/18.
-//
-
 #include "Champion.h"
 
-const Coordinate Champion::play(const Board &board) {
-    if(this->myChar == 'X'){//we start
-            for (uint x=0; x<board.size(); ++x) {
-                Coordinate c{x,x};
-                if (board[c]=='.') {
-                    return c;
-                }
-            }
-        } else{// O and  the second
+const Coordinate Champion::play(const Board& board){
 
-       int j = board.size()-2;
+	if(this->myChar == 'X'){//we start
+		for (uint x=0; x<board.size(); ++x) {
+			Coordinate c{static_cast<int>(x),static_cast<int>(x)};
+			if (board[c]=='.') {
+				return c;
+			}
+		}
+	}
+
+	else{// O and  the second
+
+		int j = board.size()-2;
 
 		Coordinate c{1,j};
 
@@ -45,15 +44,5 @@ const Coordinate Champion::play(const Board &board) {
 	return {0,0};  // did not find an empty square - play on the top-left
 
 
-    }
 
-int Champion::turnNum(const Board &board){
-    int counter=1;
-        for (int i = 0; i <board.size() ; ++i) {
-            for (int j = 0; j <board.size() ; ++j) {
-                if (board[{i,j}]!='.')
-                    counter++;
-            }
-        }
-    return counter;
 }
