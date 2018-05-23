2aa37c827f3fe93612405f8b8432d3db052ba0b5
Yarin Knafo <yarin665@gmail.com>
2018-05-23 20:06:11 +0300

    Update DummyPlayers.cpp

--- DummyPlayers.cpp
+++ DummyPlayers.cpp
@@ -2,28 +2,28 @@
 
 
 const Coordinate XYPlayer::play(const Board& board) {
-    for (uint x=0; x<board.size(); ++x) {
-        for (uint y=0; y<board.size(); ++y) {
-            Coordinate c{x,y};
-            if (board[c]=='.') {
-                return c;
-            }
-        }
-    }
-    return {0,0};  // did not find an empty square - play on the top-left
+	for (uint x=0; x<board.size(); ++x) {
+		for (uint y=0; y<board.size(); ++y) {
+			Coordinate c{static_cast<int>(x),static_cast<int>(y)};
+			if (board[c]=='.') {
+				return c;
+			}
+		}
+	}
+	return {0,0};  // did not find an empty square - play on the top-left
 }
 
 
 const Coordinate YXPlayer::play(const Board& board) {
-    for (uint y=0; y<board.size(); ++y) {
-        for (uint x=0; x<board.size(); ++x) {
-            Coordinate c{x,y};
-            if (board[c]=='.') {
-                return c;
-            }
-        }
-    }
-    return {0,0};  // did not find an empty square - play on the top-left
+	for (uint y=0; y<board.size(); ++y) {
+		for (uint x=0; x<board.size(); ++x) {
+			Coordinate c{static_cast<int>(x),static_cast<int>(y)};
+			if (board[c]=='.') {
+				return c;
+			}
+		}
+	}
+	return {0,0};  // did not find an empty square - play on the top-left
 }
 
 
@@ -32,21 +32,21 @@ const Coordinate YXPlayer::play(const Board& board) {
  * The illegal player tries to put a char on a cell owned by the other player.
  */
 const Coordinate IllegalPlayer::play(const Board& board) {
-    char charOfOtherPlayer = (
-            myChar=='X'? 'O': 'X'
-    );
-    for (uint y=0; y<board.size(); ++y) {
-        for (uint x=0; x<board.size(); ++x) {
-            Coordinate c{x,y};
-            if (board[c]==charOfOtherPlayer) {
-                return c;
-            }
-        }
-    }
-    return {0,0};  // did not find an illegal square - play on the top-left
+	char charOfOtherPlayer = (
+		myChar=='X'? 'O': 'X'
+		);
+	for (uint y=0; y<board.size(); ++y) {
+		for (uint x=0; x<board.size(); ++x) {
+			Coordinate c{static_cast<int>(x),static_cast<int>(y)};
+			if (board[c]==charOfOtherPlayer) {
+				return c;
+			}
+		}
+	}
+	return {0,0};  // did not find an illegal square - play on the top-left
 }
 
 
 const Coordinate ExceptionPlayer::play(const Board& board) {
-    throw string("hahaha");
+	throw string("hahaha");
 }
