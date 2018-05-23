1841079a5f6ad4f86a649a0509172a8a18fd48c1
Yarin Knafo <yarin665@gmail.com>
2018-05-23 23:03:50 +0300

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,7 +1,3 @@
-//
-// Created by yarin665 on 21/05/18.
-//
-
 #ifndef TTTTASK_MASTER_PLAYER_H
 #define TTTTASK_MASTER_PLAYER_H
 
@@ -10,19 +6,11 @@
 
 class Player {
 public:
-    virtual const string name() const  { return playerName; }
-
-    virtual const Coordinate play(const Board& board) ;
-
     char myChar;
-    string playerName;
-    bool won;
 
-    char getChar() const;
-
-    void setMyChar(char myChar);
-
-    Player& operator=(const Player& obj);
+    virtual const string name() const=0;
+    virtual const Coordinate play(const Board& board)=0;
+    char getChar()const {return myChar;}
 };
 
 #endif //TTTTASK_MASTER_PLAYER_H
